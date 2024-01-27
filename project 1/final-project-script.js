5080691e3b663f60acb2141569576ab0c4969a2f
Grace Chan <137463636+gracestl@users.noreply.github.com>
2024-01-27 17:09:11 +0800

    Update final-project-script.js

--- project 1/final-project-script.js
+++ project 1/final-project-script.js
@@ -1,3 +1,11 @@
+/*
+How many hours did you spend on this assignment?: +/- 6 hours
+What part of the assignment did you spend the most time on?: Customised output message, game logic and ensuring no bugs...
+How comfortable did you feel with this assignment? (1-5): 4.5 
+Is there anything in this code that you feel pleased about?: I think it works...
+What's one aspect of your code you would like specific, elaborate feedback on?: How to refactor and reduce the length and repetition.
+*/
+
 // Assigning variables to each game state
 const SCISSORS = "scissors";
 const PAPER = "paper";
@@ -6,25 +14,24 @@ const NORMAL = "normal";
 const KOREAN = "korean";
 const REVERSE = "reverse";
 
-// variable to store scores
+// Assigning variables to store scores
 let userScore = 0;
 let computerScore = 0;
 let totalGames = 0;
 
+// Assigning variables to store states
 let userPrevState = "";
 let compPrevState = "";
-
-//Starting state, blank because you want it to input something
-let userName = "";
 let gameState = "Please enter username";
+let userName = "";
 
-// Set up error message
+// Set up repeated messages
 let ERRORMESSAGE = `${userName} Please enter "scissors" "paper" or "stone" to play!`;
 let ERRORMESSAGE2 = `Please enter "normal" or "korean" to select game mode!`;
 // Set up play again message
 let PLAYAGAINMESSAGE = `<br><br>Now you can type "scissors" "paper" or "stone" to play another round!`;
 
-// Set up computerGen function and assign values to number
+// Computer generation of scissors paper or stone
 let genRandomNum = function () {
   let randomNum = Math.ceil(Math.random() * 3);
   if (randomNum == 1) {
@@ -41,6 +48,7 @@ let genRandomNum = function () {
   }
 };
 
+//Run main function
 let main = function (input) {
   // check for blank submission and return error message
   console.log("validation");
@@ -49,7 +57,7 @@ let main = function (input) {
     return "Please enter a valid username.";
   }
 
-  // check for user name
+  // check for user name and return error message
   if (gameState == "Please enter username") {
     gameState = "Select mode";
     userName = input;
@@ -57,22 +65,23 @@ let main = function (input) {
     return `Hello ${userName}! Please enter 'normal' or 'korean' to select game mode!`;
   }
 
+  // convert all input for game mode to lower case
   let modeInput = input.toLowerCase();
 
-  // check for selection of mode
+  // check for selection of mode and return error message
   if (gameState == "Select mode") {
     if (modeInput != NORMAL && modeInput != KOREAN && modeInput != REVERSE) {
       console.log(`Error2 detected`);
       return ERRORMESSAGE2;
     }
   }
-  // Select normal
+  // If user input "normal", normal mode is activated
   if (modeInput == NORMAL) {
     gameState = NORMAL;
     return `Hello ${userName}! ${gameState} mode activated! Enter scissors, paper or stone to start playing!`;
   }
 
-  // Select korean
+  // If user input "korean", korean mode is activated
   if (modeInput == KOREAN) {
     gameState = KOREAN;
     userPrevState = "";
@@ -80,18 +89,21 @@ let main = function (input) {
     return `Hello ${userName}! ${gameState} mode activated! Enter scissors, paper or stone to start playing!`;
   }
 
-  // Select reverse (secret mode)
+  // If user input "reverse", reverse mode is activated
   if (modeInput == REVERSE) {
     gameState = REVERSE;
     return `Hello ${userName}! ${gameState} mode activated! Enter scissors, paper or stone to start playing!`;
   }
 
+  // convert all user input for game to lower case
   let userInput = input.toLowerCase();
   console.log(`User chose ` + userInput);
   let computerInput = genRandomNum();
 
+  // standard message to inform what user and computer chose
   let STANDARDMESSAGE = `<br><br>The computer chose: ${computerInput}<br>You chose: ${userInput}`;
 
+  // calculation of percentage wins of user over total games
   let calculateUserPercentage = function () {
     let userPercentage = (userScore / totalGames) * 100;
     let userWins = userPercentage.toFixed(0);
@@ -99,7 +111,7 @@ let main = function (input) {
     return userWins;
   };
 
-  // Reverse mode
+  // Reverse mode gameplay to win
   if (gameState == REVERSE) {
     // Reverse mode win
     if (
@@ -114,7 +126,7 @@ let main = function (input) {
       let winnings = calculateUserPercentage();
       return `Hi ${userName}! ${STANDARDMESSAGE}<br><br>You win!<br><br>🫵🏼 score: ${userScore}<br>🤖 score: ${computerScore}<br>(You have been winning ${winnings}% of the time.)<br>${PLAYAGAINMESSAGE}`;
     }
-    // Reverse mode lose
+    // Reverse mode gameplay to lose
     if (
       (computerInput == SCISSORS && userInput == STONE) ||
       (computerInput == PAPER && userInput == SCISSORS) ||
@@ -128,7 +140,7 @@ let main = function (input) {
       return `Hi ${userName}! ${STANDARDMESSAGE}<br><br>You lose!<br><br>🫵🏼 score: ${userScore}<br>🤖 score: ${computerScore}<br>(You have been winning ${winnings}% of the time.)<br>${PLAYAGAINMESSAGE}`;
     }
   }
-  // korea mode user win
+  // Korean mode gameplay to win
   if (gameState == KOREAN && userInput == computerInput) {
     if (userPrevState == "win" && userScore > 0) {
       userScore = 0;
@@ -137,7 +149,7 @@ let main = function (input) {
       compPrevState = "";
       return `Hi ${userName}! ${STANDARDMESSAGE}<br><br>Muk-jji-ppa! You are the final winner!<br>${PLAYAGAINMESSAGE}`;
     }
-    // korea mode user lose
+    // Korean mode gameplay to lose
     if (compPrevState == "win" && computerScore > 0) {
       userScore = 0;
       computerScore = 0;
@@ -145,7 +157,7 @@ let main = function (input) {
       compPrevState = "";
       return `Hi ${userName}! ${STANDARDMESSAGE}<br><br>Muk-jji-ppa! Computer is the final winner!<br>${PLAYAGAINMESSAGE}`;
     }
-    // normal and reverse mode draw
+    // Normal and reverse mode gameplay to draw
     else userInput == computerInput;
     console.log(`User draw`);
     totalGames += 1;
@@ -153,6 +165,7 @@ let main = function (input) {
     let winnings = calculateUserPercentage();
     return `Hi ${userName}! ${STANDARDMESSAGE}<br><br>It's a draw!<br><br>🫵🏼 score: ${userScore} <br>🤖 score: ${computerScore}<br>(You have been winning ${winnings}% of the time.${PLAYAGAINMESSAGE}`;
   }
+  // Korean mode gameplay to draw without win or loss state
   if (userInput == computerInput && gameState != KOREAN) {
     console.log(`User draw`);
     totalGames += 1;
@@ -161,7 +174,7 @@ let main = function (input) {
     return `Hi ${userName}! ${STANDARDMESSAGE}<br><br>It's a draw!<br><br>🫵🏼 score: ${userScore} <br>🤖 score: ${computerScore}<br>(You have been winning ${winnings}% of the time.${PLAYAGAINMESSAGE}`;
   }
 
-  // normal mode win
+  // Normal mode gameplay to win
   if (
     (computerInput == SCISSORS && userInput == STONE) ||
     (computerInput == PAPER && userInput == SCISSORS) ||
@@ -174,7 +187,7 @@ let main = function (input) {
     winnings = calculateUserPercentage();
     return `Hi ${userName}! ${STANDARDMESSAGE}<br><br>You win!<br><br>🫵🏼 score: ${userScore}<br>🤖 score: ${computerScore}<br>(You have been winning ${winnings}% of the time.)<br>${PLAYAGAINMESSAGE}`;
   }
-  // normal mode lose
+  // Normal mode gameplay to lose
   if (
     (userInput == SCISSORS && computerInput == STONE) ||
     (userInput == PAPER && computerInput == SCISSORS) ||
@@ -188,5 +201,3 @@ let main = function (input) {
     return `Hi ${userName}! ${STANDARDMESSAGE}<br><br>You lose!<br><br>🫵🏼 score: ${userScore}<br>🤖 score: ${computerScore}<br>(You have been winning ${winnings}% of the time.)<br>${PLAYAGAINMESSAGE}`;
   }
 };
-
-garce;
